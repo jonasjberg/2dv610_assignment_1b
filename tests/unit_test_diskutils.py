0d3a303c0261f0b849fc6cd95fa08a5d00f441e8
Jonas Sjöberg <jomeganas@gmail.com>
2017-11-24 20:44:18 +0100

    [RED] Add test for 'load_file()' being defined.

---
+++ tests/unit_test_diskutils.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright(c) 2016-2017 Jonas Sjöberg
+#   Personal site:   http://www.jonasjberg.com
+#   GitHub:          https://github.com/jonasjberg
+#   University mail: js224eh[a]student.lnu.se
+#
+#   This file is part of metadatadiff.
+#
+#   metadatadiff is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation.
+#
+#   metadatadiff is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with metadatadiff.  If not, see <http://www.gnu.org/licenses/>.
+
+from unittest import TestCase
+
+import diskutils
+
+
+class TestLoadFile(TestCase):
+    def test_load_file_is_defined(self):
+        self.assertIsNotNone(diskutils.load_file)
