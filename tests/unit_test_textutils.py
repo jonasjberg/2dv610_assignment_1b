894b6462ae60bc3204d3b34c418b9ed15ab8171d
Jonas Sjöberg <jomeganas@gmail.com>
2017-11-24 15:55:10 +0100

    Fix project name in comment header license.

--- tests/unit_test_textutils.py
+++ tests/unit_test_textutils.py
@@ -5,19 +5,19 @@
 #   GitHub:          https://github.com/jonasjberg
 #   University mail: js224eh[a]student.lnu.se
 #
-#   This file is part of autonameow.
+#   This file is part of metadatadiff.
 #
-#   autonameow is free software: you can redistribute it and/or modify
+#   metadatadiff is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation.
 #
-#   autonameow is distributed in the hope that it will be useful,
+#   metadatadiff is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #   GNU General Public License for more details.
 #
 #   You should have received a copy of the GNU General Public License
-#   along with autonameow.  If not, see <http://www.gnu.org/licenses/>.
+#   along with metadatadiff.  If not, see <http://www.gnu.org/licenses/>.
 
 from unittest import TestCase
 
