20a2239e27d28edcbbdcfc9e19bdad35ffecd98e
Jonas Sjöberg <jomeganas@gmail.com>
2017-11-24 16:28:36 +0100

    [GREEN] Implement 'BaseField.__eq__()'.

--- metadatadiff/model/field.py
+++ metadatadiff/model/field.py
@@ -26,3 +26,10 @@ class BaseField(object):
 
     def __repr__(self):
         return '{!s}("{!s}")'.format(self.__class__.__name__, self.value)
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        if self.value == other.value:
+            return True
+        return False
