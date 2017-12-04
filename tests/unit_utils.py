1783b16388b5af2b5f5199c1063220b6a7cc7354
Jonas Sjöberg <jomeganas@gmail.com>
2017-12-04 15:38:25 +0100

    [GREEN] Implement skeleton 'abspath_testfile()'.

---
+++ tests/unit_utils.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+#   Copyright(c) 2016-2017 Jonas Sjöberg
+#   Personal site:   http://www.jonasjberg.com
+#   GitHub:          https://github.com/jonasjberg
+#   University mail: js224eh[a]student.lnu.se
+#
+#   This file is part of metadatadiff.
+#
+#   metadatadiff is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation.
+#
+#   metadatadiff is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with metadatadiff.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+import unit_constants as uuconst
+
+
+def abspath_testfile(basename):
+    """
+    Utility function used by tests to construct a full path to individual test
+    files in the 'test_files' directory.
+
+    Args:
+        basename: The basename of a file in the 'test_files' directory
+                  as a Unicode string (internal string format)
+
+    Returns:
+        The full absolute path to the given file.
+    """
+    pass
